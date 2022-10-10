3f03213cbf88a54111ee4d49d1c28df9ce05e8a8
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-11 00:30:50 +0300

    dobavil zadanie001

---
+++ zadanie001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int NumberDay = new Random().Next(1, 8);
+string[] Day = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+string txtResult = "";
+
+if (NumberDay > 5) txtResult = $", Выходной!!!";
+else txtResult = $", Рабочий день.";
+
+Console.WriteLine();
+Console.WriteLine($"{NumberDay} день недели - Это {Day [NumberDay - 1]}{txtResult}");
+Console.WriteLine();
+
+
